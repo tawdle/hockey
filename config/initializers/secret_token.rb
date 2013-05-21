be7f3275888910b90b81a124cce441891141ca1b
Todd Agulnick <todd@agulnick.com>
2013-05-21 15:23:22 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hockey::Application.config.secret_token = '36514e3a4ee39ddae811344511ccc142c8a1753dd124bb8fcee91390d736ee6b76663111de76e00341e942e15d3c03aac79305783acda7db62b03d73e7631f4c'
