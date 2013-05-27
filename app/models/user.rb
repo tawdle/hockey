96523fc533c52c73185bb5323b571ddd713f6843
Todd Agulnick <todd@agulnick.com>
2013-05-27 11:30:35 -0700

    Add name field to user.
    This is the @username for the user. We require that it be at least 3 characters long and no more than 20, that it contain only alphanumerics and _, and that it be case-insensitively unique amongst all users. This last bit requires some extra work because ActiveRecord doesn't support the creation of case-insensitive db indices.
    
    See http://stackoverflow.com/questions/7948501/case-insensitive-unique-index-in-rails-activerecord for details. And use rake db:structure:dump and db:structure:load to move the database structure around.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
+
+  validates_presence_of :name
+  validates_uniqueness_of :name, :case_sensitive => false
+  validates_format_of :name, :with => /\A[a-zA-Z0-9_]*\Z/, :message => 'can contain only alphanumeric characters'
+  validates_length_of :name, :within => 3..20
 end
---
+++ db/migrate/20130527174739_add_name_to_user.rb
@@ -0,0 +1,11 @@
+class AddNameToUser < ActiveRecord::Migration
+  def up
+    # See http://stackoverflow.com/questions/7948501/case-insensitive-unique-index-in-rails-activerecord
+    execute "CREATE UNIQUE INDEX index_users_on_lowercase_name
+             ON users USING btree (lower(name));"
+  end
+
+  def down
+    execute "DROP INDEX index_users_on_lowercase_name"
+  end
+end
