ecc3d8ddb849eb17c1757a09209162d2445acfc9
Todd Agulnick <todd@agulnick.com>
2013-05-29 07:46:47 -0700

    fix busted migration

--- db/migrate/20130527174739_add_name_to_user.rb
+++ db/migrate/20130527174739_add_name_to_user.rb
@@ -1,5 +1,6 @@
 class AddNameToUser < ActiveRecord::Migration
   def up
+    add_column :users, :name, :string, :null => false
     # See http://stackoverflow.com/questions/7948501/case-insensitive-unique-index-in-rails-activerecord
     execute "CREATE UNIQUE INDEX index_users_on_lowercase_name
              ON users USING btree (lower(name));"
