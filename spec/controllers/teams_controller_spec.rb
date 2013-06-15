2e13894038b5f6949ae13730ab62e001a28e3b53
Todd Agulnick <todd@agulnick.com>
2013-06-15 16:28:52 -0400

    First crack at teams_controller_spec.

---
+++ spec/controllers/teams_controller_spec.rb
@@ -0,0 +1,69 @@
+require 'spec_helper'
+
+describe TeamsController do
+  let(:team) { FactoryGirl.create(:team) }
+  let(:league) { FactoryGirl.create(:league) }
+
+  describe "#index" do
+    def do_request
+      get :index
+    end
+    it "should work" do
+      do_request
+    end
+  end
+
+  describe "#show" do
+    def do_request
+      get :show, :id => team.to_param
+    end
+    it "should work" do
+      do_request
+    end
+  end
+
+  describe "#new" do
+    def do_request
+      get :new
+    end
+    it "should work" do
+      do_request
+    end
+  end
+
+  describe "#edit" do
+    def do_request
+      get :edit, :id => team.to_param
+    end
+    it "should work" do
+      do_request
+    end
+  end
+
+  describe "#create" do
+    def do_request
+      post :create, :team => {:name => "My New Team", :league_id => league.to_param }
+    end
+    it "should work" do
+      do_request
+    end
+  end
+
+  describe "#update" do
+    def do_request
+      put :update, :id => team.to_param, :team => {:name => "My Very New Team" }
+    end
+    it "should work" do
+      do_request
+    end
+  end
+
+  describe "#destroy" do
+    def do_request
+      delete :destroy, :id => team.to_param
+    end
+    it "should work" do
+      do_request
+    end
+  end
+end
