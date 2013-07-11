a69b9defc3f71291bde1d7fd9f9bd4e83ba6868b
Todd Agulnick <todd@agulnick.com>
2013-07-11 13:30:25 -0700

    Enable sendgrid mail on production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,16 @@ Hockey::Application.configure do
   # So devise can send email
   config.action_mailer.default_url_options = { :host => 'mygameshot.com' }
 
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
   # Enable threaded mode
   # config.threadsafe!
 
