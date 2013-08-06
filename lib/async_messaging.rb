5b9b86dfb557a5ff6dad809592ac64a1d5ad3bc2
Todd Agulnick <todd@agulnick.com>
2013-08-06 13:08:35 -0700

    Implement simple broadcast via Faye.

---
+++ lib/async_messaging.rb
@@ -0,0 +1,13 @@
+module AsyncMessaging
+
+  FAYE_CONFIG = HashWithIndifferentAccess.new(YAML::load(File.read(Rails.root.join("config/faye.yml"))))[Rails.env]
+
+  def broadcast(channel, msg=nil, &block)
+    unless FAYE_CONFIG[:disabled]
+      message = {:channel => channel, :data => msg || capture(&block), :ext => {:password => FAYE_CONFIG[:password]}}
+      uri = URI.parse(FAYE_CONFIG[:uri])
+      Net::HTTP.post_form(uri, :message => message.to_json)
+    end
+  end
+end
+
