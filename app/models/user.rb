92f8a12811dd6257f78769d9a4eb53ad1cd73b21
Todd Agulnick <todd@agulnick.com>
2013-08-08 11:29:00 -0700

    Allow usernames to include -.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
 
   validates_presence_of :name
   validates_uniqueness_of :name, :case_sensitive => false
-  validates_format_of :name, :with => /\A[a-zA-Z0-9_]*\Z/, :message => 'can contain only alphanumeric characters'
+  validates_format_of :name, :with => /\A[a-zA-Z0-9_-]*\Z/, :message => 'can contain only alphanumeric characters'
   validates_length_of :name, :within => 3..20
   validates_uniqueness_of :nameable_id, :scope => :nameable_type, :allow_nil => true
 
