87c671b8f8c00c9d9576683f65049869c52ac646
Todd Agulnick <todd@agulnick.com>
2013-08-09 12:53:08 -0700

    Remove tick handling from timers.

--- app/assets/javascripts/models/timer.js
+++ app/assets/javascripts/models/timer.js
@@ -2,16 +2,65 @@ window.App = window.App || {};
 
 App.Timer = Backbone.Model.extend({
   defaults: {
-    state: "created", 
-    elapsedTime: 0.0
+    state: "created"
   },
 
-  timerId: null,
-  lastTick: null,
-
   initialize: function() {
+    this.pausedAt = null;
+    this.secondsPaused = 0.0;
     this.on("change:state", this.stateChanged, this);
-    this.trigger("change:state", this);
+    this.trigger("change:state");
+  },
+
+  get: function(key) {
+    return  key == "elapsedTime" ?
+      this.getElapsedTime() :
+      Backbone.Model.prototype.get.apply(this, arguments);
+  },
+
+  set: function(key, val, options) {
+    var attrs;
+
+    if (key === null) return this;
+    if (typeof key === 'object') {
+      attrs = key;
+      options = val;
+    } else {
+      (attrs = {})[key] = val;
+    }
+
+    options = options || {};
+
+    if (attrs.elapsedTime !== undefined) {
+      this.setElapsedTime(attrs.elapsedTime);
+      delete attrs.elapsedTime;
+    }
+
+    return Backbone.Model.prototype.set.apply(this, arguments);
+  },
+
+  setElapsedTime: function(val) {
+    var et = Number(val) * 1000.0;
+    this.baseTime = (this.pausedAt ? this.pausedAt : Date.now()) - (this.secondsPaused || 0.0) - et;
+  },
+
+  getElapsedTime: function() {
+    var et = 0.0;
+
+    switch (this.get("state")) {
+      case "created":
+        break;
+      case "finished":
+        break;
+      case "running":
+      case "finished":
+        et = (Date.now() - this.secondsPaused - this.baseTime) / 1000.0;
+        break;
+      case "paused":
+        et = (this.pausedAt - this.secondsPaused - this.baseTime) / 1000.0;
+        break;
+    }
+    return et;
   },
 
   stateChanged: function() {
@@ -19,31 +68,27 @@ App.Timer = Backbone.Model.extend({
       case "created":
         break;
       case "running":
-        this.lastTick = Date.now();
-        this.timerId = setInterval(this.tick.bind(this), 500);
+        if (this.pausedAt) {
+          this.secondsPaused += (Date.now() - this.pausedAt);
+          this.pausedAt = null;
+        } else {
+          this.baseTime = this.baseTime || Date.now();
+        }
         break;
       case "paused":
+        this.pausedAt = this.pausedAt || Date.now();
+        break;
       case "finished":
-        if (this.timerId) {
-          clearInterval(this.timerId);
-          this.timerId = null;
-        }
         break;
     }
   },
 
-  tick: function() {
-    var now = Date.now();
-    this.set("elapsedTime", Number(this.get("elapsedTime")) + (now - this.lastTick) / 1000.0);
-    this.lastTick = now;
-  },
-
   elapsedTimeHMS: function() {
-    var seconds = this.get("elapsedTime");
+    var seconds = this.getElapsedTime();
     return {
       "hours" : Math.floor(seconds / 3600),
       "minutes": Math.floor(seconds / 60) % 60,
       "seconds": Math.floor(seconds % 60)
-    }
+    };
   }
 });
