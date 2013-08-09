b72a54d9d3e2ebacc5a990b6cab3b50c44af0492
Todd Agulnick <todd@agulnick.com>
2013-08-09 12:54:59 -0700

    GameView triggers a clockTick event every 500ms.
    Everyone that cares about timing events can listenTo this single clock.

--- app/assets/javascripts/views/game_view.js
+++ app/assets/javascripts/views/game_view.js
@@ -5,6 +5,9 @@ App.GameView = Backbone.View.extend({
     this.gameStart = $("#game-start");
     this.gamePause = $("#game-pause");
     this.listenTo(this.model, "change", this.render);
+    setInterval(function() { this.trigger("clockTick"); }.bind(this), 500);
+    var el = $("#game-clock").first();
+    this.gameClockView = new App.TimerView({el: el, model: App.game.get("clock"), parent: this });
   },
 
   render: function() {
--- app/assets/javascripts/views/timer_view.js
+++ app/assets/javascripts/views/timer_view.js
@@ -1,16 +1,16 @@
 window.App = window.App || {};
 
 App.TimerView = Backbone.View.extend({
- initialize: function() {
-    this.listenTo(this.model, "change", this.render);
+  initialize: function() {
+    this.listenTo(this.options.parent, "clockTick", this.render);
   },
 
   render: function() {
     var timer = this.model;
     var hms = timer.elapsedTimeHMS();
-    var h = hms["hours"] > 0 ? (String(hms["hours"]) + ":") : "";
-    var m = (hms["minutes"] < 10 ? "0" : "") + String(hms["minutes"]) + ":";
-    var s = (hms["seconds"] < 10 ? "0" : "") + String(hms["seconds"]);
+    var h = hms.hours > 0 ? (String(hms.hours) + ":") : "";
+    var m = (hms.minutes < 10 ? "0" : "") + String(hms.minutes) + ":";
+    var s = (hms.seconds < 10 ? "0" : "") + String(hms.seconds);
     var result = h + m + s;
     if (timer.get("state") == "running" && Math.floor(timer.get("elapsedTime") * 2.0) % 2)
       result = result.replace(/:/g, " " );
