df639a530ff8942b40fd69f21c3cf915abf00c0f
Todd Agulnick <todd@agulnick.com>
2013-09-17 12:06:20 -0700

    Make penalty delete work.

--- app/assets/javascripts/views/penalty_view.js
+++ app/assets/javascripts/views/penalty_view.js
@@ -2,17 +2,26 @@ App.PenaltyView = Backbone.View.extend({
   initialize: function() {
     this.template = _.template($("#penalty-view").html());
     this.listenTo(this.model, "change", this.render);
+    this.listenTo(this.model, "destroy", this.remove, this);
   },
 
   tagName: "tr",
 
   events: {
-    "click a.edit" : "edit"
+    "click a.edit" : "edit",
+    "click a.delete" : "delete"
   },
 
   edit: function(e) {
     e.preventDefault();
     App.dispatcher.trigger("penalty:edit", this.model);
+  },
+
+  delete: function(e) {
+    e.preventDefault();
+    if (confirm("Delete this penalty?")) {
+      this.model.destroy({ wait: true });
+    }
   },
 
   render: function() {
