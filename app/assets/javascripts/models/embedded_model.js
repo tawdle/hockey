1f2d254fb779d25884fb71de1b5a211c89150d2c
Todd Agulnick <todd@agulnick.com>
2013-09-17 20:29:35 -0700

    Deal with one-to-many embedded models.
    EmbeddedModel extends Backbone.model. It allows subclasses to specify a models() function, which returns a hash. The keys in the hash specify names of attributes that come from the server (in JSON) that represent embedded models. The values identify either a Backbone model class or an instance of a Backbone collection.
    
    If the value is a Backbone model and the instance doesn't yet exist in the parent, it is instantiated with the given parameters. If the instance does already exist, we call set() on it with the given parameters.
    
    If the value is a Backbone collection, we call set() on the collection with the given list of object parameters.

---
+++ app/assets/javascripts/models/embedded_model.js
@@ -0,0 +1,47 @@
+window.App = window.App || {};
+
+App.EmbeddedModel = Backbone.Model.extend({
+  // Inherit from EmbeddedModel to allow a model to instantiate
+  // embedded models. To use it, declare a "models" hash that
+  // has a key that identifies the incoming JSON key name,
+  // and a value that corresponds to name of the Backbone model
+  // to be instantiated.
+
+  set: function(key, val, options) {
+    var o, a, attrs;
+
+    if (key === null) return this;
+    if (typeof key === 'object') {
+      attrs = key;
+      options = val;
+    } else {
+      (attrs = {})[key] = val;
+    }
+
+    options = options || {};
+
+    var models = this.models();
+    for (var model in models) {
+      a = attrs[model];
+      if (a) {
+        if (models[model] instanceof Backbone.Collection) {
+          models[model].set(a);
+          delete attrs[model];
+        } else {
+          o = this.get(model);
+          if (o) {
+            o.set(a, options);
+            delete attrs[model];
+          } else {
+            if (!(a instanceof Backbone.Model)) {
+              this.set(model, new models[model](a));
+              delete attrs[model];
+            }
+          }
+        }
+      }
+    }
+    return Backbone.Model.prototype.set.apply(this, arguments);
+  }
+});
+
