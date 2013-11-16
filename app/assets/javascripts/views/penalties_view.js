dda27e5b9723cb7b1077f726cb3a996aa49586cc
Todd Agulnick <todd@agulnick.com>
2013-11-15 21:36:35 -0800

    Show penalty counts in tabs.

--- app/assets/javascripts/views/penalties_view.js
+++ app/assets/javascripts/views/penalties_view.js
@@ -34,18 +34,30 @@ App.PenaltiesView = Backbone.CollectionView.extend({
     var self = this;
     var filter = null;
     this.teamId = options.teamId;
+    this.tab = $("a[href=#" + this.el.parentNode.id + "]");
+    this.count = $("span.count", this.tab);
     options.visibleModelsFilter = this.filters[options.type](self);
     options.modelView = App.PenaltyView;
     options.selectable = false;
     Backbone.CollectionView.prototype.initialize.apply(this, [options]);
     this.listenTo(App.game, "change:state", this.gameStateChanged);
-    $("a[href=#" + this.el.parentNode.id + "]").on("show", function() { self.render(); });
+    this.listenTo(App.penalties, "add", this.updateCount);
+    this.listenTo(App.penalties, "remove", this.updateCount);
+    this.listenTo(App.penalties, "change", this.updateCount);
+    this.tab.on("show", function() { self.render(); });
+    this.updateCount();
   },
 
   gameStateChanged: function() {
     if (this.$el.is(":visible") && App.game.get("state") == "playing") {
       this.render();
     }
+  },
+
+  updateCount: function() {
+    var self = this;
+    var count = this.collection.reduce(function(memo, penalty) { return self.options.visibleModelsFilter(penalty) ? memo + 1 : memo; }, 0);
+    this.count.text(count ? " (" + count + ")" : "");
   }
 });
 
