871d589d10918515fedd6a13ada83a4980f257e2
Todd Agulnick <todd@agulnick.com>
2013-11-15 19:21:15 -0800

    Clean up filter functions a bit.

--- app/assets/javascripts/views/penalties_view.js
+++ app/assets/javascripts/views/penalties_view.js
@@ -1,40 +1,40 @@
 App.PenaltiesView = Backbone.CollectionView.extend({
-  initialize: function(options, other) {
-    var self = this;
-    var filter = null;
-    this.teamId = options.teamId;
-    switch (options.type) {
-      case "all":
-        filter = function(penalty) {
-          return penalty.teamId() == self.teamId;
-        };
-        break;
-      case "current":
-        filter = function(penalty) {
+  filters: {
+    all: function(self) {
+      return function(penalty) { return penalty.teamId() == self.teamId; };
+    },
+    current: function(self) {
+      return function(penalty) {
           var state = penalty.get("state");
           return penalty.teamId() == self.teamId &&
             (state == "created" || state == "running" || state == "paused") &&
             penalty.get("minutes");
-        };
-        break;
-      case "expired":
-        filter = function(penalty) {
+      };
+    },
+    expired: function(self) {
+      return function(penalty) {
           var state = penalty.get("state");
           return penalty.teamId() == self.teamId &&
             (state == "completed" || state == "canceled") &&
             penalty.get("minutes");
-        };
-        break;
-      case "other":
-        filter = function(penalty) {
-          var state = penalty.get("state");
-          return penalty.teamId() == self.teamId &&
-            (state == "created") &&
-            !penalty.get("minutes");
-        };
-        break;
+      };
+    },
+    other: function(self) {
+      return function(penalty) {
+        var state = penalty.get("state");
+        return penalty.teamId() == self.teamId &&
+          (state == "created") &&
+          !penalty.get("minutes");
+      };
     }
-    options.visibleModelsFilter = filter;
+  },
+
+
+  initialize: function(options, other) {
+    var self = this;
+    var filter = null;
+    this.teamId = options.teamId;
+    options.visibleModelsFilter = this.filters[options.type](self);
     options.modelView = App.PenaltyView;
     options.selectable = false;
     Backbone.CollectionView.prototype.initialize.apply(this, [options]);
