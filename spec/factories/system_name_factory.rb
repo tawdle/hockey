64ccfeab390577aee781435554ca1f3bb6822f95
Todd Agulnick <todd@agulnick.com>
2013-12-10 13:56:40 -0800

    Fix system name specs.
    I don't really understand how the uniqueness validations work.

--- spec/factories/system_name_factory.rb
+++ spec/factories/system_name_factory.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :system_name do
     sequence(:name) { |n| "Name_#{n}" }
     after(:build) do |system_name|
-      system_name.nameable ||= FactoryGirl.build(:user, :system_name => system_name)
+      system_name.nameable ||= FactoryGirl.build(:team, :system_name => system_name)
     end
   end
 end
--- spec/models/system_name_spec.rb
+++ spec/models/system_name_spec.rb
@@ -16,13 +16,14 @@ describe SystemName do
     end
 
     it "requires a unique nameable" do
+      FactoryGirl.create(:system_name, :nameable => system_name.nameable)
       expect {
-        FactoryGirl.create(:system_name, :nameable => system_name.nameable)
+        system_name.save!
       }.to raise_error
     end
 
     describe "#name" do
-      it "should require a name" do
+      it "requires a name" do
         system_name.name = ""
         system_name.should_not be_valid
         system_name.name = nil
@@ -32,27 +33,27 @@ describe SystemName do
         FactoryGirl.create(:system_name, :name => system_name.name)
         system_name.should_not be_valid
       end
-      it "should prohibit invalid characters" do
+      it "prohibits invalid characters" do
         system_name.name = "foo*&^"
         system_name.should_not be_valid
       end
-      it "should require at least 3 chars" do
+      it "requires at least 3 chars" do
         system_name.name = "ab"
         system_name.should_not be_valid
       end
-      it "should require fewer than 20 chars" do
+      it "requires fewer than 20 chars" do
         system_name.name = "x" * 21
         system_name.should_not be_valid
       end
-      it "should allow accented characters" do
+      it "allows accented characters" do
         system_name.name = "éric"
         system_name.should be_valid
       end
-      it "should allow numbers" do
+      it "allows numbers" do
         system_name.name = "foo4u"
         system_name.should be_valid
       end
-      it "should allow _ and -" do
+      it "allows _ and -" do
         system_name.name = "_-_"
         system_name.should be_valid
       end
