6b78755f3a00def455bee8d6f53775c4232063a4
Todd Agulnick <todd@agulnick.com>
2014-01-11 13:34:54 -0800

    Remove secret token from file.
    Use Heroku environment variables instead.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Hockey::Application.config.secret_token = '36514e3a4ee39ddae811344511ccc142c8a1753dd124bb8fcee91390d736ee6b76663111de76e00341e942e15d3c03aac79305783acda7db62b03d73e7631f4c'
+Hockey::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
