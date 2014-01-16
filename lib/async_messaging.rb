6b919970ff34d49f914b8c5ce934857c2d4e1d15
Todd Agulnick <todd@agulnick.com>
2014-01-15 19:18:50 -0800

    Don't do SSL when talking to chatter from server.

--- lib/async_messaging.rb
+++ lib/async_messaging.rb
@@ -6,7 +6,8 @@ module AsyncMessaging
     unless FAYE_CONFIG[:disabled]
       #puts "AsyncMessaging: #{msg || capture(&block)}"
       message = {:channel => channel, :data => msg || capture(&block), :ext => {:password => FAYE_CONFIG[:password]}}
-      uri = URI.parse(FAYE_CONFIG[:uri])
+      # We don't really need to use https here, and the code to POST over SSL is strangely nasty
+      uri = URI.parse(FAYE_CONFIG[:uri].sub(/^https/, "http")
       Net::HTTP.post_form(uri, :message => message.to_json)
     end
   end
