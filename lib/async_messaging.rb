4011e456912bfc51c3135dd2e193bb36464a6021
Todd Agulnick <todd@agulnick.com>
2014-01-16 16:57:51 -0800

    Use ENV to keep chatter password.

--- lib/async_messaging.rb
+++ lib/async_messaging.rb
@@ -5,7 +5,7 @@ module AsyncMessaging
   def broadcast(channel, msg=nil, &block)
     unless FAYE_CONFIG[:disabled]
       #puts "AsyncMessaging: #{msg || capture(&block)}"
-      message = {:channel => channel, :data => msg || capture(&block), :ext => {:password => FAYE_CONFIG[:password]}}
+      message = {:channel => channel, :data => msg || capture(&block), :ext => {:password => ENV["FAYE_PASSWORD"]}}
       # We don't really need to use https here, and the code to POST over SSL is strangely nasty
       uri = URI.parse(FAYE_CONFIG[:uri].sub(/^https/, "http"))
       Net::HTTP.post_form(uri, :message => message.to_json)
