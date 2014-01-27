4e3d203b0deea4a904d81128bc6b01a7e43ce5cb
Todd Agulnick <todd@agulnick.com>
2014-01-26 16:31:33 -0800

    If no game, refresh scoreboard every 5 minutes.

--- app/controllers/locations/scoreboards_controller.rb
+++ app/controllers/locations/scoreboards_controller.rb
@@ -1,14 +1,16 @@
 class Locations::ScoreboardsController < ApplicationController
   load_and_authorize_resource :location
 
+  layout "scoreboard"
+
   def show
     authorize! :view_scoreboard, @location
     @game = @location.game_for_scoreboard
 
     if @game
-      render :layout => "scoreboard"
+      render
     else
-      render :template => "no_game"
+      render "no_game"
     end
   end
 end
