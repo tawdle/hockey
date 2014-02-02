0583e2aad5d47bec831d14da5222902e49fdc7c1
Todd Agulnick <todd@agulnick.com>
2014-02-02 14:40:41 -0500

    Show edit game button.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -6,4 +6,14 @@ module GamesHelper
       "<li><span class='text-error' style='margin-left: 0px; margin-right: 2px;'>&#10006;</span> <b>#{ link_to text, link }</b>".html_safe
     end
   end
+
+  def edit_game_path(game)
+    if game.nil? || game.league.nil?
+      nil
+    elsif game.league.is_a?(Tournament)
+      edit_tournament_game_path(game.league, game)
+    else
+      edit_league_game_path(game.league, game)
+    end
+  end
 end
