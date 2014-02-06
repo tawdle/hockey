93c927fe752d0a35e6f225da19d83c65ff817fae
Todd Agulnick <todd@agulnick.com>
2014-02-05 22:55:59 -0500

    Localize player claim emails.

--- app/mailers/player_claim_mailer.rb
+++ app/mailers/player_claim_mailer.rb
@@ -8,22 +8,28 @@ class PlayerClaimMailer < ActionMailer::Base
   default from: format_address("mailer@bigshot.io", "BigShot.io")
 
   def created(player_claim)
-    assign_attrs(player_claim)
-    mail to: @team.managers.map(&:email)
+    send_localized_mail(player_claim, player_claim.player.team.managers)
   end
 
   def approved(player_claim)
-    assign_attrs(player_claim)
-    mail to: [@creator.email]
+    send_localized_mail(player_claim, [player_claim.creator])
   end
 
   def denied(player_claim)
-    assign_attrs(player_claim)
-    mail to: [@creator.email]
+    send_localized_mail(player_claim, [player_claim.creator])
   end
 
   private
   
+  DEFAULT_LANGUAGE = 'fr'
+
+  def send_localized_mail(player_claim, to_users)
+    assign_attrs(player_claim)
+    I18n.with_locale(to_users.first.language || DEFAULT_LANGUAGE) do
+      mail to: to_users.map(&:email)
+    end
+  end
+
   def assign_attrs(player_claim)
     @player_claim = player_claim
     @player = player_claim.player
