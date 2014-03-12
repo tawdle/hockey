335d8c19bcd1af8e9955d178b2c4d7cd36e8c2de
Todd Agulnick <todd@agulnick.com>
2014-03-12 14:07:28 -0700

    Fix broken feed.

--- app/assets/javascripts/feed.js
+++ app/assets/javascripts/feed.js
@@ -1,11 +1,11 @@
 $(function() {
   $(".activity-feed").on("click", ".reply-link", function(e) {
     e.preventDefault();
-    $(this).hide().parent().siblings("form").slideDown(function() { $(this).find("input[type=text]").focus(); });
+    $(this).hide().parent().siblings().find("form").slideDown(function() { $(this).find("input[type=text]").focus(); });
   });
 
   $(".activity-feed").on("ajax:success", ".feed-item form", function(event, data, status, xhr) {
-    $(data).appendTo($(this).parent().find(".replies")).hide().slideDown("fast").find("time").timeago();
+    $(data).appendTo($(this).parent().parent().find(".replies")).hide().slideDown("fast").find("time").timeago();
     this.reset();
   });
 
