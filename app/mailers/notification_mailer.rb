7c4eed24f973c3f5a668f0c5e9351f70471c23dc
Todd Agulnick <todd@agulnick.com>
2014-03-24 20:40:21 -0700

    I18n for notification mail.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -11,18 +11,18 @@ class NotificationMailer < ActionMailer::Base
 
   def new_feed_items(user, items)
     attachments.inline['avatar.png'] = File.read(Rails.root.join("app/assets/images/fallback/avatar_thumbnail.png"))
-    send_localized_mail(user, items)
+    send_localized_mail(user, I18n.t("notification_mailer.new_feed_items.subject", :name => user.name), items)
   end
 
   private
 
   DEFAULT_LANGUAGE = 'fr'
 
-  def send_localized_mail(user, items)
+  def send_localized_mail(user, subject, items)
     @user = user
     @items = items
     I18n.with_locale(user.language || DEFAULT_LANGUAGE) do
-      mail to: [user.email]
+      mail to: [user.email], subject: subject
     end
   end
 end
