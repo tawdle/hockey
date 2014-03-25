2316d58527dc368a1d5df01d4af541a75d23abf8
Todd Agulnick <todd@agulnick.com>
2014-03-24 20:19:53 -0700

    Add url_opts and html_opts to link helpers.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,16 +1,16 @@
 module UsersHelper
   include Rails.application.routes.url_helpers
 
-  def format_user_link(user, klass='username')
-    "<span class='#{klass}'>@#{link_to(user.cached_system_name, user)}</span>".html_safe if user
+  def format_user_link(user, url_opts={}, html_opts={})
+    content_tag(:span, "@" + link_to(user.cached_system_name, user_path(user, url_opts), html_opts), html_opts, false).html_safe if user
   end
 
   def format_team_link(team)
     "<span class='username'>@#{link_to(team.system_name.name, team)}</span>".html_safe if team && team.system_name
   end
 
-  def format_username_link(username)
-    "<span class='username'>@#{link_to(username, system_name_path(username))}</span>".html_safe if username
+  def format_username_link(username, url_opts={}, html_opts={})
+    content_tag(:span, "@" + link_to(username, system_name_path(username, url_opts), html_opts), html_opts, false).html_safe if username
   end
 
   def format_player_link(player)
@@ -19,30 +19,30 @@ module UsersHelper
     end
   end
 
-  def format_playername_link(playername)
+  def format_playername_link(playername, url_opts={}, html_opts={})
     if playername
       teamname, jersey_number = playername.split("#")
-      "<span class='username'>@#{link_to(playername, system_name_path(teamname, :j => jersey_number))}</span>".html_safe
+      content_tag(:span, "@" + link_to(playername, system_name_path(teamname, url_opts.merge(:j => jersey_number))), html_opts, false).html_safe
     end
   end
 
-  def format_link(username, displayname)
+  def format_link(username, displayname, url_opts = {}, html_opts={})
     if username.include?("#")
       team, jersey = username.split("#")
-      dest = system_name_path(team, :j => jersey)
+      dest = system_name_path(team, url_opts.merge(:j => jersey))
     else
-      dest = system_name_path(username)
+      dest = system_name_path(username, url_opts)
     end
-    link_to(displayname, dest, :title => username)
+    link_to(displayname, dest, html_opts.merge(:title => username))
   end
 
-  def format_message_with_usernames(msg)
+  def format_message_with_usernames(msg, url_opts={}, html_opts={})
     matches = msg.scan(Mention::FeedNamePattern)
     if matches.any?
       msg = ERB::Util.html_escape(msg)
       matches.each do |username, displayname|
         source = Regexp.escape("[[@#{username} #{ERB::Util.html_escape(displayname)}]]")
-        msg.gsub!(/#{source}/, format_link(username, displayname))
+        msg.gsub!(/#{source}/, format_link(username, displayname, url_opts, html_opts))
       end
     end
 
@@ -53,9 +53,9 @@ module UsersHelper
       username_matches.each do |username|
         at_name = Regexp.escape("@#{username}")
         if username.include?("#")
-          msg.gsub!(/#{at_name}\b/, format_playername_link(username))
+          msg.gsub!(/#{at_name}\b/, format_playername_link(username, url_opts, html_opts))
         else
-          msg.gsub!(/#{at_name}\b/, format_username_link(username))
+          msg.gsub!(/#{at_name}\b/, format_username_link(username, url_opts, html_opts))
         end
       end
     end
