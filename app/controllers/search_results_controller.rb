93d0f5be0267b98449536d1bbe4271c1ce9f964a
Todd Agulnick <todd@agulnick.com>
2014-04-05 16:36:34 -0700

    Don't show deleted items in search results.

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -2,7 +2,7 @@ class SearchResultsController < ApplicationController
   skip_authorization_check
 
   def index
-    @results = PgSearch.multisearch(params[:q]).map(&:searchable)
+    @results = PgSearch.multisearch(params[:q]).map(&:searchable).reject {|i| i.respond_to?(:deleted_at) && i.deleted_at }
     @query_text = params[:q]
   end
 end
