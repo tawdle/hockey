ad5c63c0ef27e88b5bc3670c273f640278245744
Todd Agulnick <todd@agulnick.com>
2014-04-10 15:51:31 -0700

    Fix case-sensitivity bug in system_name redirects.

--- app/controllers/system_names_controller.rb
+++ app/controllers/system_names_controller.rb
@@ -2,7 +2,8 @@ class SystemNamesController < ApplicationController
   skip_authorization_check
 
   def show
-    system_name = SystemName.find_by_name!(params[:id])
+    system_name = SystemName.where("lower(name) = ?", (params[:id] || "").downcase).first
+    raise ActiveRecord::RecordNotFound unless system_name
     nameable = system_name.nameable
     if nameable.is_a?(Team) && params[:j]
       nameable = nameable.players.where(:jersey_number => params[:j]).first || nameable
